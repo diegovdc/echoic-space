2997db9afa5f666f97a95914c5b485c7b5dce82a
Diego <diego.vid.eco@gmail.com>
2019-03-30 20:42:09 -0600

    [ADD] open server route for /blog

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const R = require('ramda')
 const music = require('./reagent-frontend/public/data/music.json')
+const blog = require('./reagent-frontend/public/data/blog.json')
 const log = require('tap-logger')
 
 // console.log("(path.join(process.cwd(), './reagent-frontend/public/js')) ", (path.join(process.cwd(), './reagent-frontend/public/js')));
@@ -12,6 +13,7 @@ app.use('/robots.txt', express.static(path.join(process.cwd(), './reagent-fronte
 app.use('/js', express.static(path.join(process.cwd(), './reagent-frontend/public/js')))
 app.use('/css', express.static(path.join(process.cwd(), './reagent-frontend/public/css')))
 app.use('/music', express.static(path.join(process.cwd(), './reagent-frontend/public/music')))
+app.use('/blog', express.static(path.join(process.cwd(), './reagent-frontend/public/blog')))
 app.use('/downloads', express.static(path.join(process.cwd(), './reagent-frontend/public/downloads')))
 app.use('/images', express.static(path.join(process.cwd(), './reagent-frontend/public/images')))
 app.use('/data', express.static(path.join(process.cwd(), './reagent-frontend/public/data')))
@@ -60,6 +62,22 @@ app.get('/music', (req, res) => {
       openGraph,
     })
 })
+app.get('/blog', (req, res) => {    
+    let openGraph = {
+      url: res.locals.baseUrl + req.url,
+      title: '| Blog',
+      description: 'Ideas, procesos, textos y noticias',
+      image: {
+        url: seoBase.image.url(res),
+        width: seoBase.image.width,
+        height: seoBase.image.height,
+      }
+    }
+
+    res.render('index', {
+      openGraph,
+    })
+})
 
 app.get('/music/:work_slug', (req, res) => {    
     let work = R.pipe(
@@ -78,6 +96,34 @@ app.get('/music/:work_slug', (req, res) => {
       image: {
         url: work.seo.img 
           ? res.locals.baseUrl + '/music/' + work.slug + '/' + work.seo.img
+          : seoBase.image.url(res),
+        width: work.seo.width,
+        height: work.seo.height,
+      }
+    }
+
+    res.render('index', {
+      openGraph,
+    })
+})
+
+app.get('/blog/:entry_slug', (req, res) => {    
+    let work = R.pipe(
+      R.find(m => 
+        R.path(['attributes', 'slug'], m) 
+        === req.params.entry_slug, 
+      ),
+      R.pathOr({}, ['attributes']),
+      log,
+    )(blog)
+    let seoImage = R.pathOr({url: seoBase.image.url(res)}, ['seo'], work)
+    let openGraph = {
+      url: res.locals.baseUrl + req.url,
+      title: `| ${work.title}`,
+      description: work.seo.description || work.description,
+      image: {
+        url: work.seo.img 
+          ? res.locals.baseUrl + '/blog/' + work.slug + '/' + work.seo.img
           : seoBase.image.url(res),
         width: work.seo.width,
         height: work.seo.height,
