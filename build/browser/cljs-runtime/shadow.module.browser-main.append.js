320880e7d225c7e421ed72dfbd536de0650926a0
Diego Villaseñor <diego.vid.eco@gmail.com>
2024-01-21 14:14:10 -0600

    Fix gitignored files

--- build/browser/cljs-runtime/shadow.module.browser-main.append.js
+++
@@ -1,4 +0,0 @@
-
-shadow.cljs.devtools.client.env.module_loaded('browser-main');
-
-try { browser.main.init(); } catch (e) { console.error("An error occurred when calling (browser.main/init)"); throw(e); }
